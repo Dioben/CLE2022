e8eb809da1650c839952cff2a3a1115d63a35e6d
p-laranjinha <plcasimiro@ua.pt>
2022-03-14 23:41:47 +0000

    all but the actual calc should be done

--- 02/determinant.c
+++ 02/determinant.c
@@ -1,53 +1,81 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /*
-save current pointer
-aim to 0
-find first line with actual stuff on it, count elements
-reset pointer to previous value
-return size
+Gets file and order of the matrix
+Returns the matrix
 */
-int readDimension(FILE* file){
-    return 1;
+void readMatrix(FILE *file, int order, double matrix[order][order])
+{
+    for (int x = 0; x < order; x++)
+        for (int y = 0; y < order; y++)
+            fread(&matrix[x][y], sizeof(double *), 1, file);
 }
+
 /*
-Scan first line for length, THEN declare stuff and read into matrix, return
+Gets order of the matrix and matrix
+Returns the determinant of the matrix
+
+Supports easy resolve for small matrixes, alternatively runs Gaussian elimination
 */
-double ** readMatrix(char* file){
-    FILE *file = fopen(file, "rb");
-    if (file == NULL){
-        int matrix[1][1];
-        matrix[0][0]=0;
-        return matrix;
+double calculateDeterminant(int order, double matrix[order][order])
+{
+    if (order == 1)
+    {
+        return matrix[0][0];
     }
-    int size = readDimension(file);
-    double matrix[size][size];
-    //TODO: ACTUALLY READ MATRIX
-    return matrix;
+    if (order == 2)
+    {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]; // AD - BC
+    }
+    return -69.420;
+    // TODO: write way to calc determinant for matrices with order > 2
 }
+
 /*
-Supports easy resolve for small matrixes, alternatively runs Gaussian elim
+Given a file name, returns a list with the determinants of matrices in it
+File format:
+first 4 bytes -> int with number of matrices
+second 4 bytes -> in with order of the matrices
+rest of the file in 8 byte sections -> doubles in the matrices
 */
-double calculateDeterminant(double ** matrix){
- size_t rows = sizeof(matrix)/sizeof(matrix[0]);
- if (rows == 1){
-     return matrix[0][0];
- }
-  if (rows == 2){
-     return matrix[0][0]*matrix[1][1]- matrix[0][1]*matrix[1][0]; //AD - BC
- }
+double *parseFile(char *fileName)
+{
+    FILE *file = fopen(fileName, "rb");
+    if (file == NULL)
+        return NULL;
+    unsigned int count;
+    unsigned int order;
+    fread(&count, 4, 1, file);
+    fread(&order, 4, 1, file);
+    double *determinants = (double *) malloc((count + 1) * sizeof(double *));
+    determinants[0] = count;
+    if (determinants == NULL)
+    {
+        perror("Failed to malloc.\n");
+        exit(1);
+    }
+    for (int i = 1; i <= count; i++)
+    {
+        double matrix[order][order];
+        readMatrix(file, order, matrix);
+        determinants[i] = calculateDeterminant(order, matrix);
+    }
+    return determinants;
 }
 
 int main(int argc, char **args)
 {
-    printf("%-50s %30s\n", "File Name","Determinant");
+    printf("%-50s %6s %30s\n", "File Name", "Matrix", "Determinant");
     for (int i = 1; i < argc; i++)
     {
-        double ** matrix = readMatrix(args[i]);
-        double determinant = calculateDeterminant(matrix);
-        printf("%-50s %30.5f\n", args[i],determinant);
+        double *determinants = parseFile(args[i]);
+        int count = determinants[0];
+        for (int ii = 1; ii <= count; ii++)
+        {
+            printf("%-50s %6d %30.5f\n", args[i], ii, determinants[ii]);
+        }
+        free(determinants);
     }
-
     return 0;
 }
