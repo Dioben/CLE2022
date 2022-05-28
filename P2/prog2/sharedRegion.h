37cb2bd34a67dc5e0023e5427162ea9db7ac4db3
buckaroo69 <diogobento@ua.pt>
2022-05-28 12:05:37 +0100

    speedrun smem

---
+++ P2/prog2/sharedRegion.h
@@ -0,0 +1,77 @@
+/**
+ * @file sharedRegion.h (interface file)
+ *
+ * @brief Problem name: multithreaded dispatcher for determinant calculation
+ *
+ * Shared region acessed by 2 threads at the same time.
+ *
+ * @author Pedro Casimiro, nmec: 93179
+ * @author Diogo Bento, nmec: 93391
+ */
+
+#ifndef SHARED_REGION_H_
+#define SHARED_REGION_H_
+
+#include <stdbool.h>
+
+/**
+ * @brief Struct containing the results calculated from a file.
+ *
+ * "marixCount" - number of matrices in the file.
+ * "determinants" - array with the determinant of all matrices.
+ */
+typedef struct Result
+{
+    int matrixCount;
+    double *determinants;
+} Result;
+
+/** @brief Number of files to be processed. */
+extern int totalFileCount;
+
+/** @brief Array with the file names of all files. */
+extern char **files;
+
+/**
+ * @brief Initializes the shared region.
+ *
+ * Needs to be called before anything else in this file.
+ *
+ * @param _totalFileCount number of files to be processed
+ * @param _files array with the file names of all files
+ */
+extern void initSharedRegion(int _totalFileCount, char *_files[]);
+
+/**
+ * @brief Frees all memory allocated during initialization of the shared region or the results.
+ *
+ * Should be called after the shared region has stopped being used.
+ */
+extern void freeSharedRegion();
+
+
+/**
+ * @brief Initializes the result of a file.
+ *
+ * @param matrixCount number of matrices in the file
+ */
+extern void initResult(int matrixCount);
+
+
+/**
+ * @brief Gets the results of all files.
+ *
+ * @return Result struct array with all the results
+ */
+extern Result *getResults();
+
+/**
+ * @brief Allows merger to get a result object, will block until result at index has been initialized
+ *
+ * @param fileIndex index of the file
+ * @param matrixIndex index of the matrix in the file
+ * @param determinant determinant of the matrix
+ */
+extern Result* getResultToUpdate(int idx);
+
+#endif
