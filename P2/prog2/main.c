6faf46b27ca6c09c714a2ad470baf4c45f1b13f5
buckaroo69 <diogobento@ua.pt>
2022-05-29 11:04:30 +0100

    main commented

--- P2/prog2/main.c
+++ P2/prog2/main.c
@@ -1,7 +1,7 @@
 /**
  * @file main.c (implementation file)
  *
- * @brief Problem name: multiprocess determinant calculation
+ * @brief Problem name: multiprocess determinant calculation with multithreaded dispatcher
  *
  *
  * @author Pedro Casimiro, nmec: 93179
@@ -139,7 +139,10 @@ static void printResults(char** fileNames, int fileCount){
 /**
  * @brief Main thread.
  *
- * Its role is generating the worker threads, waiting for their termination, and printing the end result.
+ * Determines whether it is a worker or dispatcher, performs associated tasks
+ * Dispatchers are multi-threaded and output results
+ * Dispatcher threads include a file reader, a sending component, and a results merger
+ * Worker performs tasks until it receives an exit signal
  *
  * @param argc argument count
  * @param args argument array
@@ -149,9 +152,12 @@ int main(int argc, char **args)
 {
 
     int rank, size;
-
+    
+    //MPI threading support afforded to us
     int provided;
+
     MPI_Init_thread(&argc, &args, MPI_THREAD_MULTIPLE, &provided);
+    
     if (provided < MPI_THREAD_MULTIPLE) {
         printf("The threading support level is lesser than demanded.\n");
         MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
@@ -175,18 +181,16 @@ int main(int argc, char **args)
         {
             int stop = 0;
             for (int i = 1; i < size; i++)
-                // signal that there's nothing left to process
+                // signal to workers that there's nothing left to process
                 MPI_Send(&stop, 1, MPI_INT, i, 0, MPI_COMM_WORLD); 
             MPI_Finalize();
             return EXIT_FAILURE;
         }
-        int fileCount = cmdArgs.fileCount;
-        char **fileNames = cmdArgs.fileNames;
 
         struct timespec start, finish;              // time measurement
         clock_gettime(CLOCK_MONOTONIC_RAW, &start); // begin time measurement
 
-        initSharedRegion(fileCount, fileNames,size,10);
+        initSharedRegion(cmdArgs.fileCount, cmdArgs.fileNames,size,10);
         
         //create reader thread
         pthread_t reader;
@@ -205,7 +209,7 @@ int main(int argc, char **args)
             exit(EXIT_FAILURE);
         }
 
-         //create dispatcher thread
+        //create sender thread
         pthread_t sender;
         if (pthread_create(&sender, NULL, emitTasksToWorkers, NULL) != 0)
             {
